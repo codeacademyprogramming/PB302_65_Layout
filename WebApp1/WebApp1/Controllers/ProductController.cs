3b4eb7f5ab9a532a20176eea401979067b7b2e38
Elchin Aghayev <Elchin@code.local>
2024-04-16 21:31:34 +0400

    project added

---
+++ WebApp1/WebApp1/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private List<Product> _products = new List<Product>();
+        public ProductController()
+        {
+            _products = new List<Product>
+             {
+                 new Product
+            {
+                Id = 1,
+                Name = "Pr 1",
+                Price = 250
+            },
+                 new Product
+            {
+                Id = 2,
+                Name = "Pr 2",
+                Price = 450
+            },
+                 new Product
+            {
+                Id = 3,
+                Name = "Pr 3",
+                Price = 550
+            }
+        };
+        }
+
+        public ContentResult info()
+        {
+            return Content("fdf");
+        }
+        public ActionResult Index()
+        {
+            if (_products == null || _products.Count == 0)
+            {
+                TempData["ErrorMessage"] = "Products not found";
+                return RedirectToAction("error");
+            }
+
+            ViewBag.Products = _products;
+
+            return View();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var product = _products.Find(x => x.Id == id);
+            if (product == null)
+            {
+                TempData["ErrorMessage"] = "Product not found by given id";
+                return RedirectToAction("error");
+            }
+
+            ViewData["product"] = product;
+
+            return View();
+        }
+
+        public ViewResult Error()
+        {
+
+            return View();
+        }
+    }
+}
